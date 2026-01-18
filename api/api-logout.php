5deb33668acea89f91523156c045d1ef1a0d2813
Davide Rossi <davide.rossi47@studio.unibo.it>
2026-01-18 18:34:47 +0100

    add redirect to logout

--- api/api-logout.php
+++ api/api-logout.php
@@ -1,12 +1,13 @@
 <?php
 require_once("../PHPUtilities/bootstrap.php");
-header('Content-Type: application/json');
-
 if (isUserLoggedIn()) {
     $_SESSION = [];
     session_destroy();
-    echo json_encode(['response' => 'Logout avvenuto con successo']);
+    http_response_code(200);
+    header("Location: ../pages/index.php");
 } else {
+    header('Content-Type: application/json');
+    http_response_code(401);
     echo json_encode(['error' => 'Nessun utente loggato']);
 }
 ?>
