dacbcdf90cabdc790d180efee4ae4c4bf74f6594
EnricoAncaraniUnibo <enrico.ancarani@studio.unibo.it>
2026-01-19 17:29:59 +0100

    afs

--- PHPPages/navbar.php
+++ PHPPages/navbar.php
@@ -1,9 +1,9 @@
 <nav class="navbar navbar-expand navbar-layout">
         <div class="container-lg d-flex">
-            <a class="navbar-brand navbar-title" href="PHPPages/homepageUser.php">Uni Events</a>
+            <a class="navbar-brand navbar-title" href="homepageUser.php">Uni Events</a>
             <ul class="navbar-nav flex-row justify-content-around w-75">
                 <li class="nav-item">
-                    <a href="PHPPages/homepageUser.php" class="navbar-button navbar-button-active">
+                    <a href="homepageUser.php" class="navbar-button navbar-button-active">
                         <img src="../img/home_icon.svg" alt="Home" class="small-icon"/>
                         <span class="d-none d-sm-inline ms-2">Home</span>
                     </a>
@@ -36,7 +36,7 @@
                         <ul class="list-unstyled">
                             <li class="d-flex dropdown-item align align-items-center">
                                 <img src="../img/activity_icon.svg" alt="Le mie attività" class="small-icon"/>
-                                <a class="ms-2 dropdown-link" href="PHPPages/mieAttivita.php">Le mie attività</a>
+                                <a class="ms-2 dropdown-link" href="mieAttivita.php">Le mie attività</a>
                             </li>
                             <li class="d-flex dropdown-item align align-items-center">
                                 <img src="../img/profile_icon.svg" alt="Mio profilo" class="small-icon"/>
@@ -48,7 +48,7 @@
                             </li>
                             <li class="d-flex dropdown-item align align-items-center">
                                 <img src="../img/dashboard_icon.svg" alt="Bacheca" class="small-icon"/>
-                                <a class="ms-2 dropdown-link" href="PHPPages/bachecaAdmin.php">Bacheca</a>
+                                <a class="ms-2 dropdown-link" href="bachecaAdmin.php">Bacheca</a>
                             </li>
                             <li><hr class="dropdown-divider"></li>
                             <li class="d-flex dropdown-item align align-items-center">
